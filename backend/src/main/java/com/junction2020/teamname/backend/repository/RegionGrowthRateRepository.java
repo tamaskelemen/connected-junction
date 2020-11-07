8d73ca502ff3d49ef90677f28ed585c0da856886
Csaba Ilonka <24225461+csaba-ilonka@users.noreply.github.com>
2020-11-07 04:05:50 +0100

    Testing GeoJson read/write/persist/load capabilities.

---
+++ backend/src/main/java/com/junction2020/teamname/backend/repository/RegionGrowthRateRepository.java
@@ -0,0 +1,8 @@
+package com.junction2020.teamname.backend.repository;
+
+import com.junction2020.teamname.backend.model.RegionGrowthRate;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RegionGrowthRateRepository extends CrudRepository<RegionGrowthRate, String> {
+
+}
