294ce72dd467a8c6764ebcdf5e339695654ae248
Csaba Ilonka <24225461+csaba-ilonka@users.noreply.github.com>
2020-11-07 02:02:24 +0100

    Fixed start script.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pkill -f 'java -jar'
 git pull
-./mvnw package
-nohup java -jar target/demo-0.0.1-SNAPSHOT.war &
+./gradlew backend:clean assemble --console=plain
+nohup java -jar backend/build/libs/backend-1.0-SNAPSHOT.jar &
