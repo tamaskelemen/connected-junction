26d4841a1dd28d79a7042a7e7bd56cd33e10e10b
Csaba Ilonka <24225461+csaba-ilonka@users.noreply.github.com>
2020-11-07 02:10:46 +0100

    Set start script to run with the prod profile.

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 pkill -f 'java -jar'
 git pull
 ./gradlew backend:clean assemble --console=plain
-nohup java -jar backend/build/libs/backend-1.0-SNAPSHOT.jar &
+nohup java -jar -Dspring.profiles.active=prod backend/build/libs/backend-1.0-SNAPSHOT.jar &
