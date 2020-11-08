25bd211c597a736ad81a90eeb8f1f5d9f6403fe8
polyakz <polyakzoltan@outlook.com>
2020-11-08 02:59:33 +0100

    I don't love ya at all! #2213

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -18,7 +18,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/light-v9',
+    id: 'mapbox/streets-v9',
     tileSize: 512,
     zoomOffset: -1
 }).addTo(mapview);
